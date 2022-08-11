4ea56c9952f551b2d067b70bc403371ffbf54ca0
Pedro Henrique Bruneli de Melo <pmelo@safira.local>
2022-08-11 17:52:24 -0300

    Likes fixed

--- front-end/movies-app/src/app/movies-big-slot/movies-big-slot.component.ts
+++ front-end/movies-app/src/app/movies-big-slot/movies-big-slot.component.ts
@@ -31,37 +31,49 @@ export class MoviesBigSlotComponent implements OnInit {
   }
 
   onClick($event: any) {
-    const id = $event.currentTarget.id;
+    const id = $event.currentTarget.dataset.internalid;
     const elements = document.getElementsByClassName(`.${id}`);
-    let liked = false;
+    let likedFirebase = false;
+    let likedText = false;
     for (let i = 0; i < elements.length; i++) {
       const element = elements.item(i);
       const likes = element.querySelector('ion-text');
       element.querySelectorAll('fa-icon').forEach((icon) => {
-        if (!liked) {
-          liked = true;
-          this.changeLikeButtonAndText(icon, likes, id);
-        }
         icon.classList.toggle('disable');
+        if (!likedFirebase) {
+          this.changeLikeButton(icon, id);
+          likedFirebase = true;
+        }
+        if (!likedText) {
+          this.changeText(icon, likes);
+          likedText = true;
+        }
       });
+      likedText = false;
     }
   }
 
-  changeLikeButtonAndText(
-    icon: Element,
-    likes: HTMLIonTextElement,
-    id: number
-  ) {
-    const likesValue = Number(likes.textContent.split(' Likes')[0]);
+  changeLikeButton(icon: Element, id: number) {
     if (
       icon.classList.contains('regular') &&
       icon.classList.contains('disable')
     ) {
-      this.service.removeLike(id);
-      likes.textContent = likesValue - 1 + ' Likes';
-    } else {
       this.service.addLike(id);
-      likes.textContent = likesValue + 1 + ' Likes';
+    } else {
+      this.service.removeLike(id);
+    }
+  }
+
+  changeText(icon: Element, likes: HTMLIonTextElement) {
+    if (
+      icon.classList.contains('regular') &&
+      icon.classList.contains('disable')
+    ) {
+      const likesValue = Number(likes.textContent.split(' Likes')[0]) + 1;
+      likes.textContent = likesValue + ' Likes';
+    } else {
+      const likesValue = Number(likes.textContent.split(' Likes')[0]) - 1;
+      likes.textContent = likesValue + ' Likes';
     }
   }
 
